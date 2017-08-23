fb1b686f19d6de8155b7f5e1721dfaeff663c0a2
Christian Graf <christian.graf@cyberfox.at>
2017-08-23 16:57:05 +0200

    chore(*): add (c) statement to each PHP file

--- packages/container/src/Exceptions/PropertyNotFoundException.php
+++ packages/container/src/Exceptions/PropertyNotFoundException.php
@@ -1,4 +1,12 @@
 <?php
+/*
+ * ----------------------------------------------------------------------------
+ * This file is part of the Redfox PHP Framework
+ * and is subject to the MIT license that is bundled with this source code.
+ *
+ * @copyright (c) 2017 Cyberfox Software Solutions e.U.
+ * ----------------------------------------------------------------------------
+ */
 
 namespace Redfox\Container\Exceptions;
 
